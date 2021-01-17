86987b32e900975a08c30c3ea3d1869f4b180650
Sergii Ostraczerta <skylitik@outlook.com>
2021-01-17 10:20:04 +0100

    changes

---
+++ TikTakToe/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTakToe
+{
+    public enum Player
+    {
+        None = 0,
+        Crosses,
+        Noughts
+    }
+    public struct Square(Player owner)
+    
+    }
+}
